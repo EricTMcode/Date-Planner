cb1841782fcf4f2a28c6ec137bc66ecff3db9e91
Eric <ebagnaro@gmail.com>
2023-11-20 15:02:46 +0100

    Initial Commit

---
+++ Date Planner/Date_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_PlannerApp.swift
+//  Date Planner
+//
+//  Created by Eric on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Date_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
